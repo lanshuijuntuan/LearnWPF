38028c79fdc60e32a64a0eb08f59b225354ac951
zhq <724598042@qq.com>
2017-03-16 18:35:42 +0800

    提交WPF使用Task操作UI的示例

--- LearnWPF/Task/TaskDemo.HowToUseContinueWith/Program.cs
+++ LearnWPF/Task/TaskDemo.HowToUseContinueWith/Program.cs
@@ -14,31 +14,40 @@ namespace TaskDemo.HowToUseContinueWith
             Action<string> action = (str) =>
                 {
                     Console.WriteLine("current task sleep two seconds (that return no result)");
-                    System.Threading.Thread.Sleep(2000);
+                    System.Threading.Thread.Sleep(10000);
                     Console.WriteLine("Task={0},Thread={1},Str={2}", Task.CurrentId, Thread.CurrentThread.ManagedThreadId, str);
                 };
 
             Console.WriteLine("Creating a sequence of action tasks (that return no result)");
             Task.Factory.StartNew(() => action("alpha")).
                 ContinueWith(antecendent => action("beta")).
-                ContinueWith(antecendent => action("gama"));
+                ContinueWith(antecendent => action("gama")).Wait();
 
             Func<int, int> negate =
             (n) =>
             {
                 Console.WriteLine("current task sleep two seconds (that return result)");
-                System.Threading.Thread.Sleep(2000);
+                System.Threading.Thread.Sleep(10000);
                 Console.WriteLine("Task={0},Thread={1},n={2},-n={3}", Task.CurrentId, Thread.CurrentThread.ManagedThreadId, n.ToString(), -n);
                 return -n;
             };
 
             Console.WriteLine("Creating a sequence of action tasks (that return result)");
-            Task.Factory.StartNew<int>(() => negate(5)).ContinueWith(p => negate(p.Result)).ContinueWith(p => negate(p.Result));
+            Task.Factory.StartNew<int>(() => negate(5)).
+                ContinueWith(p => negate(p.Result)).
+                ContinueWith(p => negate(p.Result)).Wait();
 
             Console.WriteLine("Creating a Mix task");
             Task.Factory.StartNew<int>(() => negate(88)).
                 ContinueWith(antecendent => action("test")).
-                ContinueWith(antecendent => negate(66));
+                ContinueWith(antecendent => negate(66)).Wait();
+
+            Console.WriteLine("主线程开始执行工作......");
+
+            Thread.Sleep(2000);
+            
+
+            Console.WriteLine("主线程完成工作......");
 
             Console.ReadLine();
             
---
+++ LearnWPF/Task/ThreadPoolDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadPoolDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int workerThread=0;
+            int completionThread=0;
+            ThreadPool.GetMaxThreads( out workerThread, out completionThread);
+            Console.WriteLine(string.Format("当前ThreadPool.GetMaxThreads()线程数目,workerThread：{0};completionThread：{1}", workerThread, completionThread));
+            for (int i = 0; i < 5; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(TestThread2),i);
+            }
+
+            Console.WriteLine(string.Format("当前ThreadPool.GetMaxThreads()线程数目,workerThread：{0};completionThread：{1}", workerThread, completionThread));
+
+            Console.ReadLine();
+        }
+
+
+        public  static void TestThread(object test)
+        {
+            Console.WriteLine(string.Format("当前线程ID:{0}", Thread.CurrentThread.ManagedThreadId));
+            int workerThread = 0;
+            int completionThread = 0;
+            Console.WriteLine(string.Format("当前ThreadPool.GetMaxThreads()线程数目,workerThread：{0};completionThread：{1}", workerThread, completionThread));
+        }
+
+
+        public static void TestThread2(object num)
+        {
+            Console.WriteLine(string.Format("当前线程ID:{0},num:{1}", Thread.CurrentThread.ManagedThreadId,num));
+            Thread.Sleep((10 - (int)num) * 1000);
+            int workerThread = 0;
+            int completionThread = 0;
+            Console.WriteLine(string.Format("TestThread2当前ThreadPool.GetMaxThreads()线程数目,workerThread：{0};completionThread：{1},num:{2}", workerThread, completionThread,num));
+           
+        }
+    }
+}
