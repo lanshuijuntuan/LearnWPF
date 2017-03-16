9b2565a41a8718cc5b834f1369115446c44e5ab2
lanshuijuntuan <724598042@qq.com>
2017-03-16 23:59:40 +0800

    update listview demo

--- LearnWPF/ListViewDemo/MainWindow.xaml.cs
+++ LearnWPF/ListViewDemo/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -22,6 +23,99 @@ namespace ListViewDemo
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void tbxName_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void checkboxHasJob_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox mCheckBox = e.OriginalSource as CheckBox;
+            ContentPresenter mContentPresenter = mCheckBox.TemplatedParent as ContentPresenter;
+            Student stu = mContentPresenter.Content as Student;
+            MessageBox.Show(string.Format("选中学生姓名为：{0}，年级为：{1}", stu.Name, stu.Grade));
+
+            ListViewItem mListViewItem = this.listviewitems.ItemContainerGenerator.ContainerFromItem(stu) as ListViewItem;
+
+            CheckBox cb = FindVisualChild<CheckBox>(mListViewItem);
+
+            MessageBox.Show(cb.Name + "，" + cb.Height.ToString() + "，" + cb.Width.ToString());
+
+        }
+
+        private ChildType FindVisualChild<ChildType>(DependencyObject obj) where ChildType : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is ChildType)
+                {
+                    return child as ChildType;
+                }
+                else
+                {
+                    ChildType childOfChildren = FindVisualChild<ChildType>(child);
+                    if (childOfChildren != null)
+                    {
+                        return childOfChildren;
+                    }
+                }
+            }
+            return null;
+
+        }
+
+        private void StuSort(ListView lv, string sortby, ListSortDirection direction)
+        {
+            ICollectionView view = CollectionViewSource.GetDefaultView(lv.ItemsSource);
+            view.SortDescriptions.Clear();
+            SortDescription sd = new SortDescription(sortby, direction);
+            view.SortDescriptions.Add(sd);
+            view.Refresh();
+        }
+
+        private void listviewitems_Click(object sender, RoutedEventArgs e)
+        {
+            GridViewColumnHeader header = e.OriginalSource as GridViewColumnHeader;
+            
+            StuSort(listviewitems, "Score", ListSortDirection.Descending);
+        }
+    }
+
+    public class Student
+    {
+        public string ID
+        {
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Grade
+        {
+            get;
+            set;
+        }
+        public string Professional
+        {
+            get;
+            set;
+        }
+        public bool HasJob
+        {
+            get;
+            set;
+        }
+
+        public float Score
+        {
+            get;
+            set;
         }
     }
 }
