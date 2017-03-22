85648d4ba819f02bdc10c2f57b2a84f3cba628b7
lanshuijuntuan <724598042@qq.com>
2017-03-22 22:42:42 +0800

    调整目录，增加示例

---
+++ LearnWPF/DataBinding/ElementBindingDemo4/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ElementBindingDemo4
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            users.Add(new User() { Name = "张三" });
+            users.Add(new User() { Name = "李四" });
+            users.Add(new User() { Name = "王麻子" });
+            this.lb_user.ItemsSource = users;
+        }
+
+        ObservableCollection<User> users = new ObservableCollection<User>();
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            users.Add(new User() { Name = "新用户" });
+        }
+
+        private void btn_edit_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.lb_user.SelectedItem!=null)
+            {
+                (this.lb_user.SelectedItem as User).Name = "Random Name";
+            }
+        }
+
+        private void btn_del_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.lb_user.SelectedItem != null)
+            {
+               users.Remove((this.lb_user.SelectedItem as User));
+            }
+        }
+    }
+
+    public class User:INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if(value!=this.name)
+                {
+                    this.name = value;
+                    NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ LearnWPF/DataBinding/ElementBindingDemo5/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ElementBindingDemo5
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class YesNoConvertToBool : IValueConverter
+    {
+
+
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if(value.ToString().ToLower()=="yes")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if(value is bool)
+            {
+                if((bool)value)
+                {
+                    return "Yes";
+                }
+                else
+                {
+                    return "No";
+                }
+            }
+            return "No";
+        }
+    }
+
+}
