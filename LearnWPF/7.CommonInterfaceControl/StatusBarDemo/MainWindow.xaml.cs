84a625ff3db6867d7bddaaafd6187a4356524421
lanshuijuntuan <724598042@qq.com>
2017-03-26 21:17:09 +0800

    add: add demo examples

---
+++ LearnWPF/7.CommonInterfaceControl/StatusBarDemo/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StatusBarDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtEdit_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            int row = txtEdit.GetLineIndexFromCharacterIndex(txtEdit.CaretIndex);
+            int col = txtEdit.CaretIndex - txtEdit.GetCharacterIndexFromLineIndex(row);
+            lbcurpostion.Text = "Line " + (row + 1).ToString() + ",Char " + (col + 1).ToString();
+
+        }
+    }
+}
---
+++ LearnWPF/8.RichTextControl/FlowDocumentReaderDemo2/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlowDocumentReaderDemo2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FlowDocument fd = new FlowDocument();
+            Paragraph mParagraph = new Paragraph(new Run("Hello,World!"));
+            mParagraph.FontSize = 36;
+            fd.Blocks.Add(mParagraph);
+
+            mParagraph = new Paragraph(new Run("The ultimate programming greeting"));
+            mParagraph.FontSize = 14;
+            mParagraph.FontStyle = FontStyles.Italic;
+            mParagraph.TextAlignment = TextAlignment.Left;
+            mParagraph.Foreground = Brushes.Gray;
+            fd.Blocks.Add(mParagraph);
+
+            fdReader.Document = fd;
+
+
+        }
+    }
+}
