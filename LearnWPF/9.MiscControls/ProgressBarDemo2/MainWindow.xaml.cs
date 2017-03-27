9f32a22552498d42206efdc41c1fa4076d8e9fb9
lanshuijuntuan <724598042@qq.com>
2017-03-28 01:20:12 +0800

    add:添加其他杂项控件示例

---
+++ LearnWPF/9.MiscControls/ProgressBarDemo2/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProgressBarDemo2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+            worker.RunWorkerAsync();
+
+        }
+
+        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.pbStatus.Value = e.ProgressPercentage;
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+           for(int i=0;i<=100;i++)
+           {
+               var worker = sender as BackgroundWorker;
+               if(worker!=null)
+               {
+                   worker.ReportProgress(i);
+               }
+               i++;
+               System.Threading.Thread.Sleep(100);
+           }
+        }
+    }
+}
---
+++ LearnWPF/9.MiscControls/WebBrowserControlDemo/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebBrowserControlDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            wbrowser.Navigate("http://www.baidu.com");
+        }
+
+        private void Back_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbrowser.GoBack();
+        }
+
+        private void Forward_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbrowser.GoForward();
+        }
+
+        private void Go_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbrowser.Navigate(txtUrl.Text);
+        }
+
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key==Key.Enter)
+            {
+                var txtbox=sender as TextBox;
+                wbrowser.Navigate(txtbox.Text);
+            }
+        }
+
+        private void CanBack_Executed(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if(wbrowser!=null&&wbrowser.CanGoBack)
+            {
+               e.CanExecute = true;
+            }
+        }
+
+        private void CanForward_Executed(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if(wbrowser!=null&&wbrowser.CanGoForward)
+            {
+                e.CanExecute = true;
+            }
+        }
+    }
+}
