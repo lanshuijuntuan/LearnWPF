7c2745fb7f3d625eb192ab3042892498ff0ed666
lanshuijuntuan <724598042@qq.com>
2017-03-29 00:32:01 +0800

    update: 增加listbox示例

---
+++ LearnWPF/10.TabControl/TabControlDemo3/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TabControlDemo3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void pre_click(object sender, RoutedEventArgs e)
+        {
+            var index = tbSample.SelectedIndex-1;
+            if(index<0)
+            {
+                index = this.tbSample.Items.Count - 1;
+            }
+            this.tbSample.SelectedIndex = index;
+        }
+
+        private void next_click(object sender, RoutedEventArgs e)
+        {
+            var index = tbSample.SelectedIndex + 1;
+            if(index>=this.tbSample.Items.Count)
+            {
+                index = 0;
+            }
+            this.tbSample.SelectedIndex = index;
+        }
+
+        private void selected_click(object sender, RoutedEventArgs e)
+        {
+            var tab = tbSample.SelectedItem as TabItem;
+            if(tab!=null)
+            {
+                MessageBox.Show(tab.Header.ToString(), "selected tab");
+            }
+        }
+    }
+}
---
+++ LearnWPF/11.ListControls/ItemsControlDemo1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Collections.ObjectModel;
+
+namespace ItemsControlDemo1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ObservableCollection<TodoItem> TodoItems = new ObservableCollection<TodoItem>();
+            TodoItems.Add(new TodoItem() { Title = "wpf", Completion = 50 });
+            TodoItems.Add(new TodoItem() { Title = "c#", Completion = 64 });
+            TodoItems.Add(new TodoItem() { Title = "winform", Completion = 76 });
+            itemsCtrl.ItemsSource = TodoItems;
+        }
+    }
+
+    public class TodoItem
+    {
+        public string Title { get; set; }
+
+
+        public int Completion { get; set; }
+    }
+}
